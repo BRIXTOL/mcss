16468fce3ced83aec2c04fa8712933dabb1f5bb4
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-09-16 02:08:00 +0200

    remove ignored

--- lib/processor.d.ts
+++
@@ -1,6 +0,0 @@
-import { SCSSProcessor } from '../types/types';
-/**
- * Processor passed to the `rollup-plugin-scss` plugin
- * option `processor`
- */
-export declare const processor: (options: SCSSProcessor) => (scss: string) => Promise<string>;
