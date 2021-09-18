a93bec7fd7a243dd8bf0903f118aad0a2d55f84b
Nikolas Savvidis <nicos.savidis@gmail.com>
2021-09-18 10:50:56 +0200

    use custom log actions

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,9 +1,42 @@
-import { Console } from 'console';
+import chalk from 'chalk';
 
 /**
- * Console Instance - Used for stdout and stderr
+ * Plugin name
+ *
+ * All logs are prefixed with the plugin name
  */
-export const { log, error, warn } = new Console({
-  stdout: process.stdout,
-  stderr: process.stderr
-});
+const name = chalk.blueBright.bold('mcss');
+
+/**
+ * Action Log
+ */
+export const log = (message: string | string[]) => console.log(
+  typeof message === 'string'
+    ? chalk.blueBright(name, message)
+    : chalk.white(chalk.gray('     -'), message.join(chalk.gray('\n     - ')))
+);
+
+/**
+ * Info Logs
+ */
+export const info = (message: string) => console.info(
+  chalk.gray(name, message)
+);
+
+/**
+ * Warning Logs
+ */
+export const warn = (message: string) => console.warn(
+  chalk.yellowBright(name, message)
+);
+
+/**
+ * Error Logs
+ */
+export const error = (message: string, code?: string) => console.error(
+  chalk`${name} {redBright ${message}}
+
+    {red ${code}}
+
+  `
+);
